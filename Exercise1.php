c1116cddff7f1d2bfe9d4bf8f33bb099fa8af42f
RizareJohnDale <84859875+RizareJohnDale@users.noreply.github.com>
2022-09-30 13:06:54 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title> Exercise 1 </title>
+</head>
+<body bgcolor = "orange">
+<center>
+<h1>
+	My Exercise 1 in PHP
+</h1>
+<br>
+<?php
+
+	//declaring variables
+	$full_name = "John Dale D. Rizare";
+	$college_program = "Bachelor of Science in Computer Engineering";
+	$year = "3rd Year";
+	$section = "Web Design and Development Lab - 58043";
+	$string = "Today, I will learn PHP programming.";
+	
+	//print variables
+	echo "Name: " .$full_name; //This will display full name
+	echo "<br>";
+	echo "<br>";
+	echo "College Program: " .$college_program; //This will display the college program
+	echo "<br>";
+	echo "<br>";
+	echo "Year Level: " .$year; //This will display the year level
+	echo "<br>";
+	echo "<br>";
+	echo "Section: " .$section; //This will display the section
+	echo "<br>";
+	echo "<br>";
+	echo $string; //This will display the string
+	
+?>
+
+</body>
+</html>
