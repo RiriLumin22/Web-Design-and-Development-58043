23ebc99eb47ce6ddd4bba91869aa9a754544966e
RizareJohnDale <84859875+RizareJohnDale@users.noreply.github.com>
2022-11-24 19:30:40 +0800

    Add files via upload

--- Login.php
+++ Login.php
@@ -4,22 +4,25 @@
 </head>
 <body bgcolor = "orange" align = "center">
 <form action = "connection.php" method = "POST">
+<div style="border: 10px solid #555;">
+<h1> <font face = "cooper"> Login </font></h1>
+<br>
+<br>
 	<p>
-		<font color = "blue" face = "Lobster Two" size ="28">
-		<label>Username</label>
+		<font color = "blue" face = "cooper" size = "5">
+		<label>Username:</label>
 		<input type = "text" name = "User"/>
 		</font>
 	</p>
 	<p>
-		<font color = "blue" face = "Lobster Two" size ="28">
-		<label>Password</label>
+		<font color = "blue" face = "cooper" size = "5">
+		<label>Password:</label>
 		<input type = "Password" name = "text"/>
 		</font>
 	</p>
 	<p>
 		<input type = "submit" name = "Login"/>
-	</p>
-	
+	</p>	
 	
 	
 
